34274903d460e086f4d7fd438592877a119d1266
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-04-11 13:09:49 -0300

    [init] - Inicia projeto

---
+++ MeLevaAi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeLevaAi
+//
+//  Created by Jean Ramalho on 11/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
