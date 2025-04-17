ef00642f311aef015646c4a661c11dece876982e
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-04-17 20:40:05 -0300

    [feat] - Adiciona assets de icone e logo, cria struct de cores

---
+++ MeLevaAi/Resources/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  MeLevaAi
+//
+//  Created by Jean Ramalho on 17/04/25.
+//
+import Foundation
+import UIKit
+
+public struct Colors {
+    
+    static let defaultYellow = UIColor(red: 253/255, green: 218/255, blue: 13/255, alpha: 1)
+    static let darkPrimary =  UIColor(red: 0/255, green: 4/255, blue: 13/255, alpha: 1)
+    static let darkSecondary = UIColor(red: 26/255, green: 29/255, blue: 37/255, alpha: 1)
+}
+
