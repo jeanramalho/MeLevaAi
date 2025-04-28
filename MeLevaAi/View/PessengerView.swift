5071f3f0f0dd04f5903f1054f026f3a159025709
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-04-28 14:46:02 -0300

    [feat] - Implementa estrutura principal de layout da tela do passageiro

--- MeLevaAi/View/PessengerView.swift
+++ MeLevaAi/View/PessengerView.swift
@@ -6,8 +6,26 @@
 //
 import Foundation
 import UIKit
+import MapKit
 
 class PessengerView: UIView {
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    lazy var callCarButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Solicitar Carro", for: .normal)
+        button.backgroundColor = Colors.darkSecondary
+        button.setTitleColor(Colors.defaultYellow, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        button.layer.cornerRadius = 12
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,10 +44,19 @@ class PessengerView: UIView {
     }
     
     private func setHierarchy(){
-        
+        addSubview(mapView)
+        addSubview(callCarButton)
     }
     
     private func setConstraints(){
         
+        mapView.setConstraintsToParent(self)
+        
+        NSLayoutConstraint.activate([
+            callCarButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18),
+            callCarButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -18),
+            callCarButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -30),
+            callCarButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
     }
 }
