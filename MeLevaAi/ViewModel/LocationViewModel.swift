419b8262aab16032e4fdef34760a95cadb9c5cd0
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-04-29 17:41:40 -0300

    [feat] - Prepara a viewmodel para passar a atualizacao da localizacao para viewcontroller

--- MeLevaAi/ViewModel/LocationViewModel.swift
+++ MeLevaAi/ViewModel/LocationViewModel.swift
@@ -6,20 +6,38 @@
 //
 import Foundation
 import UIKit
+import CoreLocation
 import MapKit
 
-class LocationViewModel  {
+class LocationViewModel: NSObject  {
     
     private var locationManager = CLLocationManager()
     
-    public func setupViewModel(){
-        
+    // Closure que vai ser chamada quando a localização for atualizada
+    var onLocationUpdate: ((CLLocationCoordinate2D) -> Void)?
+    
+    override init() {
+        super.init()
         locationManager.delegate = self
+    }
+    
+    public func setupViewModel(){
+
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 }
 
-extension LocationViewModel: MKMapViewDelegate, CLLocationManagerDelegate {
+extension LocationViewModel: CLLocationManagerDelegate {
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        if let coordinates = locations.last?.coordinate {
+            onLocationUpdate?(coordinates)
+        }
+        
+        
+    }
 }
