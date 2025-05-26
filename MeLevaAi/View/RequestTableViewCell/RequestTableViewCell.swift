4856c87e16af81b5bf3a0709250f70b9bc6fea11
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-05-26 11:01:23 -0300

    [feat] - Cria estrutura de célula customizada das requisiocoes na view do motorista

---
+++ MeLevaAi/View/RequestTableViewCell/RequestTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RequestTableViewCell.swift
+//  MeLevaAi
+//
+//  Created by Jean Ramalho on 26/05/25.
+//
+import Foundation
+import UIKit
+
+class RequestTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "RequestTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+    }
+    
+}
