84fac017bff08a5d0dd822eab10d4ce26e3dbac8
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-05-29 15:02:46 -0300

    [feat] - Inicia cálculo da distancia entre passageiro e motorista

--- MeLevaAi/Model/UserRequestModel.swift
+++ MeLevaAi/Model/UserRequestModel.swift
@@ -5,10 +5,21 @@
 //  Created by Jean Ramalho on 01/05/25.
 //
 import Foundation
+import CoreLocation
 
 struct UserRequestModel {
     var email: String
     var nome: String
     var latitude: String
     var longitude: String
+    
+    // converte as strings das coordenadas para CLLocationCoordinate2D
+    var coordinate: CLLocationCoordinate2D? {
+        guard
+            let lat = Double(latitude),
+            let long = Double(longitude)
+        else {return nil}
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+                
+    }
 }
