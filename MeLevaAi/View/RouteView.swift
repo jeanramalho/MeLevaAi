4d371b3d2749be5c45da266867c8244046e22973
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-05-30 11:07:06 -0300

    [feat] - Cria view da tela de rota e confirmação da carona

--- MeLevaAi/View/RouteView.swift
+++ MeLevaAi/View/RouteView.swift
@@ -10,5 +10,42 @@ import MapKit
 
 class RouteView: UIView {
     
+    lazy var routeMapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
     
+    lazy var confirmRequestButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Confirmar Carona", for: .normal)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        addSubview(routeMapView)
+        addSubview(confirmRequestButton)
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
 }
