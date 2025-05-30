a3ddb32878b695991c0d679095d50194d0d9f2ec
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-05-30 11:20:20 -0300

    [feat] - finaliza estrutura da controladora da view de rota

--- MeLevaAi/Controllers/RouteViewController.swift
+++ MeLevaAi/Controllers/RouteViewController.swift
@@ -11,5 +11,28 @@ class RouteViewController: UIViewController {
     
     private let contentView: RouteView = RouteView()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
     
+    private func setup(){
+        
+        setupContentView()
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setupContentView(){
+        
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(contentView)
+    }
+    
+    private func setConstraints(){
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.setConstraintsToParent(self.view)
+    }
 }
