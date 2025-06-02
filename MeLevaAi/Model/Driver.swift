5f39a9c22fcbe8f9b19abd657c9b92165596e771
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-06-02 16:07:36 -0300

    [feat] - Cria Model com dados basicos do motorista

--- MeLevaAi/Model/Driver.swift
+++ MeLevaAi/Model/Driver.swift
@@ -5,7 +5,13 @@
 //  Created by Jean Ramalho on 02/06/25.
 //
 import Foundation
+import CoreLocation
 
 struct Driver {
+    let email: String
+    let nome: String
+    let latitude: String
+    let longitude: String
     
+    var coordinate:
 }
