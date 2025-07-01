8d2227f90ca4e20114f3084cb04f0b54f3ad9d47
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-07-01 19:11:29 -0300

    [feat] - Inicia implementação de estrutura do layout da feature de escolha de destino do passageiro

--- MeLevaAi/View/PessengerView.swift
+++ MeLevaAi/View/PessengerView.swift
@@ -65,7 +65,7 @@ class PessengerView: UIView {
     lazy var destinyLocationCircleView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .lightGray
         view.layer.cornerRadius = 8
         return view
     }()
@@ -143,7 +143,18 @@ class PessengerView: UIView {
             destinyView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
             destinyView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
             destinyView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
-            destinyView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.2),
+            destinyView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
+            
+            destinyMainStackView.topAnchor.constraint(equalTo: destinyView.topAnchor, constant: 8),
+            destinyMainStackView.leadingAnchor.constraint(equalTo: destinyView.leadingAnchor, constant: 8),
+            destinyMainStackView.trailingAnchor.constraint(equalTo: destinyView.trailingAnchor, constant: -8),
+            destinyMainStackView.bottomAnchor.constraint(equalTo: destinyView.bottomAnchor, constant: -8),
+            
+            currentLocationCircleView.heightAnchor.constraint(equalToConstant: 16),
+            currentLocationStackView.widthAnchor.constraint(equalToConstant: 16),
+            
+            destinyLocationCircleView.heightAnchor.constraint(equalToConstant: 16),
+            destinyLocationCircleView.widthAnchor.constraint(equalToConstant: 16),
             
             callCarButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 18),
             callCarButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -18),
